74f003ff317a19c731503ce7dd68d3fed7be6013
badrhrm <badr.eddine.hrimech@proton.me>
2024-04-26 11:50:55 +0100

    added menuBar containing menus(file-edit...) which also contains various menu items(open-save-find...)

--- src/main/java/com/mycompany/tp/text/editer/TPTextEditer.java
+++ src/main/java/com/mycompany/tp/text/editer/TPTextEditer.java
@@ -5,6 +5,10 @@
 package com.mycompany.tp.text.editer;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 /**
  *
@@ -12,6 +16,7 @@ import javax.swing.JFrame;
  */
 public class TPTextEditer{
     private JFrame frame;
+    private JMenuBar menuBar;
     
     public TPTextEditer(){
         frame = new JFrame();
@@ -20,6 +25,46 @@ public class TPTextEditer{
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
+        
+        
+        
+        menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        JMenuItem exitMenuItem = new JMenuItem("Exit Program");
+        fileMenu.add(newMenuItem);
+        fileMenu.add(openMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.add(exitMenuItem);
+        
+        JMenu editMenu = new JMenu("Edit");
+        JMenuItem copyMenuItem = new JMenuItem("Copy");
+        JMenuItem cutMenuItem = new JMenuItem("Cut");
+        JMenuItem pasteMenuItem = new JMenuItem("Paste");
+        JMenuItem findMenuItem = new JMenuItem("Find");
+        JMenuItem replaceMenuItem = new JMenuItem("Replace");
+        editMenu.add(copyMenuItem);
+        editMenu.add(cutMenuItem);
+        editMenu.add(pasteMenuItem);
+        editMenu.add(findMenuItem); 
+        editMenu.add(replaceMenuItem);  
+        
+        JMenu styleMenu = new JMenu("Style");
+        JMenuItem boldMenuItem = new JMenuItem("Bold");
+        JMenuItem italicMenuItem = new JMenuItem("Italic");
+        JMenuItem underlineMenuItem = new JMenuItem("Underline");
+        styleMenu.add(boldMenuItem);
+        styleMenu.add(italicMenuItem);
+        styleMenu.add(underlineMenuItem);
+        
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(styleMenu);
+        
         frame.setVisible(true);
     }
 
